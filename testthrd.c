284e83890a9f1b47ca43bdc38e252e9c2ae949fe
Malegr0 <schoening.andreas@gmx.de>
2021-11-29 09:55:51 +0100

    added array thread

--- testthrd.c
+++ testthrd.c
@@ -1,23 +1,33 @@
+// Start an array of threads
+
 #include <stdio.h>
 #include <pthread.h>
+
+#define COUNT 9 //eigentlich in h-Datei
 
 void* ThrdFunc(void *arg)
 {
 	int* numb = (int*) arg;
 	// myTyp* P = (myTyp*) arg; // damit lässt sich Struktur übergeben (also mehrere)
 	printf("\nThread number = %2d", *numb);
+	//sleep(3);
 	//return 0;
 }
 
 void main(void)
 {
-	//int j = 7;
-	pthread_t ID = 0;
-	
-	pthread_create (&ID, NULL, &ThrdFunc, &ID); 	// &ID -> fortlaufende Nummer startet mit 1
+	pthread_t ID[COUNT];
+	int j;
+	for(j = 0; j < COUNT; j++)
+	{
+		pthread_create (&ID[j], NULL, &ThrdFunc, &ID[j]); 	// &ID -> fortlaufende Nummer startet mit 1
 												// NULL -> Initialisierungsparameter, hier Standardparameter
 												// &ThrdFunc -> Funktion, die übergeben wird, die ausgeführt wird
 												// Adresse
-	printf("\naus main(): %d", ID);
-	pthread_join(ID, NULL);						// wartet bis Thread mit der jeweiligen ID fertig ist
+		printf("\naus main(): %d", ID[j]);
+	}
+	for(j = 0; j < COUNT; j++)
+	{
+		pthread_join(ID[j], NULL);						// wartet bis Thread mit der jeweiligen ID fertig ist
+	}
 }
